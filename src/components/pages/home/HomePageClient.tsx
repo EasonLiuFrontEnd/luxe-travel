92f3210d404f91b6e4dbd44342e4415e80cd2c7a
annelin27 <annelin27@gmail.com>
2025-09-15 03:12:40 +0800

    feat: add Advantage component

--- src/components/pages/home/HomePageClient.tsx
+++ src/components/pages/home/HomePageClient.tsx
@@ -4,6 +4,7 @@ import Banner from '@/components/pages/home/Banner'
 import { BookShelfSection, RecommendationSection } from '@/components/pages/home/ClassicItinerary'
 import ServiceProcess from '@/components/pages/home/ServiceProcess'
 import CollectionRecommendation from '@/components/pages/home/CollectionRecommendation'
+import Advantage from '@/components/pages/home/Advantage'
 import Feedback from '@/components/pages/home/Feedback'
 
 const HomePageClient = () => {
@@ -14,6 +15,7 @@ const HomePageClient = () => {
         <BookShelfSection />
         <RecommendationSection />
       </div>
+      <Advantage />
       <CollectionRecommendation />
       <Feedback />
       <ServiceProcess />
